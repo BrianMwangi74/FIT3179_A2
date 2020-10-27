d9e4b475161a024ab14c7025d9f3eb4829bbe437
BrianMwangi74 <54968862+BrianMwangi74@users.noreply.github.com>
2020-10-27 16:15:06 +1100

    Update styles.cc

--- css/styles.cc
+++ css/styles.cc
@@ -1,12 +1,12 @@
-#choropleth_map {
-	left: 100px;
-	right: 100px;
-	height: 500px;
-}
 #lifeExp_scatter_plot {
 	left: 100px;
 	right: 100px;
 	height: 400px;
+}
+#choropleth_map {
+	left: 100px;
+	right: 100px;
+	height: 500px;
 }
 
 body{
