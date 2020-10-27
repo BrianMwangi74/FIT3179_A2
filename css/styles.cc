a2b037df8885e89379631afe21b306624534d352
BrianMwangi74 <54968862+BrianMwangi74@users.noreply.github.com>
2020-10-27 16:09:10 +1100

    Update styles.cc

--- css/styles.cc
+++ css/styles.cc
@@ -1,1 +1,14 @@
+#choropleth_map {
+	left: 100px;
+	right: 100px;
+	height: 500px;
+}
+#lifeExp_scatter_plot {
+	left: 100px;
+	right: 100px;
+	height: 400px;
+}
 
+body{
+    font-family: 'Source Sans Pro', sans-serif;
+}
