a0385aada0fff799e80219c29e8946fae7a8dab9
BrianMwangi74 <54968862+BrianMwangi74@users.noreply.github.com>
2020-10-28 17:18:58 +1100

    Update styles.cc

--- css/styles.cc
+++ css/styles.cc
@@ -8,6 +8,16 @@
 	right: 100px;
 	height: 500px;
 }
+#lifeExp_area_chart {
+	left: 100px;
+	right: 100px;
+	height: 300px;
+}
+#gdp_area_chart {
+	left: 100px;
+	right: 100px;
+	height: 300px;
+}
 
 body{
     font-family: 'Source Sans Pro', sans-serif;
