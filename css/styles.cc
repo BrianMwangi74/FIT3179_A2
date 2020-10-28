17ce89f254668acd87a6bde78151a24477f02b7e
BrianMwangi74 <54968862+BrianMwangi74@users.noreply.github.com>
2020-10-28 19:00:04 +1100

    Update styles.cc

--- css/styles.cc
+++ css/styles.cc
@@ -1,22 +1,27 @@
 #lifeExp_scatter_plot {
 	left: 100px;
 	right: 100px;
-	height: 400px;
+	height: 350px;
 }
 #choropleth_map {
 	left: 100px;
 	right: 100px;
-	height: 500px;
+	height: 450px;
 }
 #lifeExp_area_chart {
 	left: 100px;
 	right: 100px;
-	height: 300px;
+	height: 250px;
 }
 #gdp_area_chart {
 	left: 100px;
 	right: 100px;
-	height: 300px;
+	height: 250px;
+}
+#suicideRate_scatter_plot {
+	left: 100px;
+	right: 100px;
+	height: 350px;
 }
 
 body{
